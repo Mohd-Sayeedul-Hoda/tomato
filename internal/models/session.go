061fdbc349cfa4e04300967cb01bf5dc047a431b
Sayeed <sayeedulhoda@gmail.com>
2025-11-30 00:00:47 +0530

    update model

--- internal/models/session.go
+++ internal/models/session.go
@@ -5,29 +5,35 @@ import (
 )
 
 type Session struct {
-	ID                int64      `json:"id"`
-	Label             string     `json:"label"`
-	WorkDuration      int64      `json:"work_duration"`
-	BreakDuration     int64      `json:"break_duration"`
-	LongBreakDuration int64      `json:"long_break_duration"`
-	LongBreakCycle    *int64     `json:"long_break_cycle,omitempty"`
-	StartTime         time.Time  `json:"start_time"`
-	EndTime           *time.Time `json:"end_time,omitempty"`
-	Status            string     `json:"status"`
-	SessionEstimate   *int64     `json:"session_estimate,omitempty"`
-	IsTracked         *bool      `json:"is_tracked,omitempty"`
-	Note              *string    `json:"note,omitempty"`
-	CreatedAt         *time.Time `json:"created_at,omitempty"`
+	ID              int64      `json:"id"`
+	Label           string     `json:"label"`
+	Status          string     `json:"status"`
+	SessionEstimate *int64     `json:"session_estimate,omitempty"`
+	IsTracked       *bool      `json:"is_tracked,omitempty"`
+	Note            *string    `json:"note,omitempty"`
+	CreatedAt       *time.Time `json:"created_at,omitempty"`
+	UpdatedAt       *time.Time `json:"updated_at,omitempty"`
+}
+
+type TimeProfile struct {
+	ID                int64  `json:"id"`
+	Name              string `json:"name"`
+	WorkDuration      int64  `json:"work_duration"`
+	BreakDuration     int64  `json:"break_duration"`
+	LongBreakDuration int64  `json:"long_break_duration"`
+	LongBreakCycle    int64  `json:"long_break_cycle"`
+	IsDefault         bool   `json:"is_default"`
 }
 
 type SessionCycle struct {
-	ID        int64      `json:"id"`
-	SessionID int64      `json:"session_id"`
-	Type      *string    `json:"type,omitempty"`
-	StartTime *time.Time `json:"start_time,omitempty"`
-	EndTime   *time.Time `json:"end_time,omitempty"`
-	Duration  *int64     `json:"duration,omitempty"`
-	Status    *string    `json:"status,omitempty"`
+	ID             int64      `json:"id"`
+	SessionID      int64      `json:"session_id"`
+	TimerProfileID int64      `json:"timer_profile_id"`
+	Type           *string    `json:"type,omitempty"`
+	StartTime      *time.Time `json:"start_time,omitempty"`
+	EndTime        *time.Time `json:"end_time,omitempty"`
+	Duration       *int64     `json:"duration,omitempty"`
+	Status         *string    `json:"status,omitempty"`
 }
 
 type SessionCycleWithMetadata struct {
